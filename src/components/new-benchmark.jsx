dea5af629ae2125ff2b54708929d5741a0f27896
Elad Karni <ekarni@problemsolutions.net>
2016-01-27 15:55:52 -0500

    form validation in the works

--- src/components/new-benchmark.jsx
+++ src/components/new-benchmark.jsx
@@ -22,7 +22,12 @@ module.exports = React.createClass({
       CPU: '',
       GPU: '',
       RAM: '',
-      showModal: false
+      UsernameStyle: '',
+      CPUStyle: '',
+      GPUStyle: '',
+      RAMStyle: '',
+      showModal: false,
+      disabled: true,
     }
   },
   render: function() {
@@ -42,40 +47,90 @@ module.exports = React.createClass({
           <form>
             <Row className="show-grid">
               <Col>
+                <Input hasFeedback type="text" label="Username" bsStyle={this.state.UsernameStyle} onChange={this.handleChange.bind(this, 'Username')} value={this.state.Username} bsSize="large" ref="inptUser" placeholder="Enter Username"/>
+              </Col>
+            </Row>
+            <Row className="show-grid">
+              <Col>
                 <Well bsSize="small" className="text-center">
                   Enter Basic Computer Specs
                 </Well>
-                <Input type="text" label="CPU" onChange={this.handleChange.bind(this, 'CPU')} value={this.state.CPU} bsSize="large" ref="inpCPU" placeholder="Enter Current CPU" />
+                <Input hasFeedback type="text" label="CPU" bsStyle={this.state.CPUStyle} onChange={this.handleChange.bind(this, 'CPU')} value={this.state.CPU} bsSize="large" ref="inptCPU" placeholder="Enter Current CPU" />
               </Col>
             </Row>
             <Row className="show-grid">
               <Col>
-                <Input type="text" label="GPU" onChange={this.handleChange.bind(this, 'GPU')} value={this.state.GPU} bsSize="large" placeholder="Enter Current GPU" />
+                <Input type="text" label="GPU" bsStyle={this.state.GPUStyle} onChange={this.handleChange.bind(this, 'GPU')} value={this.state.GPU} bsSize="large" ref="inptGPU" placeholder="Enter Current GPU" />
               </Col>
             </Row>
             <Row className="show-grid">
               <Col>
-                <Input type="text" label="RAM" onChange={this.handleChange.bind(this, 'RAM')} value={this.state.RAM} bsSize="large" placeholder="Enter Current RAM" />
+                <Input type="text" label="RAM" bsStyle={this.state.RAMStyle} onChange={this.handleChange.bind(this, 'RAM')} value={this.state.RAM} bsSize="large" ref="inptRAM" placeholder="Enter Current RAM" />
               </Col>
             </Row>
           </form>
         </Grid>
       </Modal.Body>
       <Modal.Footer>
-        <Button type="submit" bsStyle="primary" onClick={this.formSubmit}>Submit</Button>
+        <Button type="submit"  onClick={this.formSubmit} >Submit</Button>
         <Button bsStyle="danger" onClick={this.props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>);
   },
   formSubmit: function() {
-    var submitJSON = {};
-    submitJSON = '{ "Username":"PostTest" , "CPU":"Skylake" , "GameId": 2 }';
+    console.log("formSubmit");
+    if (this.state.UsernameStyle == 'success' && this.state.CPUStyle == 'success') {
 
-    console.log(submitJSON);
+      var submitJSON = {
+        "Username": this.state.Username,
+        "CPU": this.state.CPU,
+        "GPU": this.state.GPU,
+        "RAM": this.state.RAM,
+        "GameId": this.props.id
+      };
+      console.log(submitJSON);
+      //Actions.postBenchmarks(submitJSON);
+    } else {
+      console.log("else");
+      if (this.state.UsernameStyle !== 'seccess') {
+        console.log("No Username");
+      } else if (this.state.CPUStyle !== 'seccess') {
+        console.log("No CPU");
+      } else if (this.state.GPUStyle !== 'seccess') {
+        console.log("No GPU");
+      } else if (this.state.RAMStyle !== 'seccess') {
+        console.log("No RAM");
+      }
+    }
   },
   handleChange: function(name, e) {
-      var state = {};
-      state[name] = e.target.value;
-      this.setState(state);
+    var state = {};
+    state[name] = e.target.value;
+    this.setState(state);
+    var namestyle  = name + 'Style';
+    this.setState(this.validationState(namestyle, e));
+  },
+  validationState: function(style, event) {
+    var length = event.target.value.length;
+
+    switch (style) {
+      case 'UsernameStyle':
+        if (length > 6) this.setState({UsernameStyle: 'success'});
+        else if (length > 4) this.setState({UsernameStyle: 'warning'});
+        break;
+      case 'CPUStyle':
+        if (length > 6) this.setState({CPUStyle: 'success'});
+        else if (length > 4) this.setState({CPUStyle: 'warning'});
+        break;
+      case 'GPUStyle':
+        if (length > 6) this.setState({GPUStyle: 'success'});
+        else if (length > 4) this.setState({GPUStyle: 'warning'});
+        break;
+      case 'RAMStyle':
+        if (length > 6) this.setState({RAMStyle: 'success'});
+        else if (length > 4) this.setState({RAMStyle: 'warning'});
+        break;
+    }
+    return {style};
   }
 });
