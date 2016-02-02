3b5ded9d0dd052996870677ddaee7b983b34a96e
Elad Karni <ekarni@problemsolutions.net>
2016-02-02 16:01:54 -0500

    added search functionality to the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
 var Router = require('react-router');
+var Bootstrap = require('react-bootstrap');
+
+var Button = Bootstrap.Button;
 var Link = Router.Link;
+var Input = Bootstrap.Input;
 
 module.exports = React.createClass({
+  getInitialState() {
+    return {
+      searchedItem: ''
+    }
+  },
+  search() {
+    console.log("Searched");
+  },
   render: function() {
     return <div className="row panel panel-defualt">
       <div className="col-md-8 col-md-offset-2">
@@ -10,14 +22,21 @@ module.exports = React.createClass({
           <Link to="/">Welcome to Project Sophia</Link>
         </h2>
         <div className="input-group searchBar">
-          <input type="text" className="form-control" />
+          <Input type="text" className="form-control" onChange={this.handleChange()} value={this.state.Username} />
           <span className="input-group-btn">
-            <button className="btn btn-default" type="button">
+            <Button onClick={this.search}>
               Search
-            </button>
+            </Button>
           </span>
         </div>
     </div>
   </div>
-  }
+  },
+  handleChange: function() {
+    var state = {};
+    state[name] = e.target.value;
+    this.setState(state);
+    var namestyle  = name + 'Style';
+    this.setState(this.validationState(namestyle, e));
+  },
 })
