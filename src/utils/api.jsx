23cf36aa45808db36a54c4012c781211efd1a69f
Elad Karni <ekarni@problemsolutions.net>
2016-02-03 10:36:24 -0500

    added api url and key

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,13 +1,13 @@
 var Request = require('superagent');
 var Promise = require('es6-promise').Promise;
 
-var rootUrl = 'http://localhost:51982/api/';
-
+var rootUrl = 'https://www.igdb.com/api/v1/';
+var apiKey = '2sU8MDGHrJ6Fs069eKuS5j0W-KS5Fnl6QLaxGIlKP6A';
 module.exports = window.api = {
   get: function(url) {
     return new Promise(function(resolve, reject) {
       Request(rootUrl + url, function(err, res){
-        resolve(JSON.parse(res.text))
+        resolve(JSON.parse(res))
       });
     });
   },
